48b75811201509e0eaec231b9e3c375845a2d226
rubenvandenbosch <ruben.vdenbosch@gmail.com>
2021-11-10 14:08:40 +0100

    Changed displayInstructions function to load files from one directory based on consistent file names

--- displayInstructions.m
+++ displayInstructions.m
@@ -1,33 +1,53 @@
-function ex = displayInstructions(ex, slides)
+function ex = displayInstructions(ex, instructionsDir, slides, varargin)
+%
+% INPUTS
+% ex              : struct; experiment parameters
+% instructionsDir : char; directory containing image files of instruction
+%                   slides
+% slides          : num array; which slide(s) to display
+% stage           : char; which experiment stage to display experiment
+%                   slides from. If not specified, it is based on the value
+%                   of ex.stage
+% -------------------------------------------------------------------------
 
-% ex: experiment parameters
-% slides: which slide(s) to display
+% Process input arguments
+assert(nargin < 5, 'Too many input arguments')
+if nargin == 4; stage = varargin{1}; end
+assert(isstruct(ex), 'Input ex should be type struct')
+assert(ischar(instructionsDir), 'Input instructionsDir should be type char')
+assert(isnumeric(slides), 'Input slides should be a numeric')
+assert(ischar(stage), 'Input stage should be type char')
 
-% Instructions in folders:
-% Instructions_MVC, File: Dia01.jpg
-% Instructions_HandgripExercise, Files: Dia01.jpg - Dia10.jpg
-% Instructions_AppleTask, Files: Dia01.jpg - Dia10.jpg
-
-[pictsPath, ~, ~] = fileparts(mfilename('fullpath'));
-
-% i.e., 'MVC', 'Hexercise' or 'ChoiceTask'
-pictsPath = fullfile(pictsPath, sprintf('Instructions_%s',ex.stage));
-   
+% Prepare screen if necessary
 if ~isfield(ex, 'scr')
-   ex.scr=prepareScreen(ex); 
+    ex.scr=prepareScreen(ex);
 end
 
-for n=1:numel(slides)   
-   filename = fullfile(pictsPath,sprintf('Dia%02d_%s.jpg',slides(n),ex.language));
-   if ~exist(filename,'file')
-      break
-   end
-   image = imread(filename);
-   Screen('PutImage', ex.scr.w, image);
-   Screen('Flip', ex.scr.w);
-   WaitSecs(2);
-   myKbWait(ex);
-   WaitSecs(0.5);
+% Get experiment stage
+if ~exist(stage,'var')
+    stage = ex.stage;
+end
+
+% Display selected instruction slides
+for n = 1:numel(slides)
+    % Get file name
+    filename = fullfile(instructionsDir,sprintf('instructions_%s_%s_%d.jpg',ex.language,stage,slides(n)));
+    
+    % If requested file does not exist, warn and continue
+    if ~(exist(filename,'file') == 2)
+        warning('Instructions file does not exist: %s', filename)
+        continue
+    end
+    
+    % Read and display image
+    image = imread(filename);
+    Screen('PutImage', ex.scr.w, image);
+    Screen('Flip', ex.scr.w);
+    
+    % Wait and wait for key press
+    WaitSecs(2);
+    myKbWait(ex);
+    WaitSecs(0.5);
 end
 
 
