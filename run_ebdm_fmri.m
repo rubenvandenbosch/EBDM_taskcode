6bc91b5db7de6b5ed59f25b14d41a19df1ed0ad0
rubenvandenbosch <ruben.vdenbosch@gmail.com>
2021-11-11 13:41:50 +0100

    Changed output file names, using session instead of visit. changes to ex struct. Minor input fix

--- run_ebdm_fmri.m
+++ run_ebdm_fmri.m
@@ -1,18 +1,15 @@
 function result = run_ebdm_fmri(varargin)
-% run_ebdm_fmri([subject, visit, stage])
+% run_ebdm_fmri([subject, session, stage])
 %
 % SUMMARY
 % Script to run the effort-based decision-making (EBDM) task.
-% 
-% The output file naming convention is/will be adjusted, and is now also
-% separated by visit number to the lab.
 % 
 % Place and run this file from the root directory of the task code.
 %
 % INPUT
 % Required info, either supplied directly or asked for interactively:
 %   subject
-%   visit (1 baseline / 2 postintervention)
+%   session (1 = baseline / 2 = postintervention)
 %   stage (Experiment stage:  
 %           - practice : calibration (MVC), familiarize effort levels, 
 %                        practice choices
@@ -35,115 +32,98 @@ start_apple_task();
 % Main directories
 % -------------------------------------------------------------------------
 % Root directory of task code
-ex.rootDir = fileparts(mfilename('fullpath'));
+ex.dirs.rootDir = fileparts(mfilename('fullpath'));
 
 % Output directory for result files
 %       For now, use the same as it was (i.e. "output" one level up from 
 %       root task code dir)
-ex.outputFolder = fullfile(ex.rootDir,'..','output');
+ex.dirs.output = fullfile(ex.dirs.rootDir,'..','output');
 
 % Create output directory if it does not exist
-if ~exist(ex.outputFolder,'dir')
-    mkdir(ex.outputFolder); 
+if ~exist(ex.dirs.output,'dir')
+    mkdir(ex.dirs.output); 
 end
 
 % Get subject, visit, and experiment stage info
 % -------------------------------------------------------------------------
 % If applicable, use provided input arguments
 if nargin == 3
-    ex.subjectId = varargin{1};
-    ex.visit     = varargin{2};
+    ex.subject  = varargin{1};
+    ex.session   = varargin{2};
     ex.stage     = varargin{3};
 elseif nargin == 2
-    ex.subjectId = varargin{1};
-    ex.visit     = varargin{2};
+    ex.subject   = varargin{1};
+    ex.session   = varargin{2};
     ex.stage     = lower(input('Experiment stage: ','s'));
 elseif nargin == 1
-    ex.subjectId = varargin{1};
-    ex.visit     = input('Visit number: ');
+    ex.subject   = varargin{1};
+    ex.session   = input('Visit number: ');
     ex.stage     = lower(input('Experiment stage: ','s'));
 else
-    ex.subjectId = input('Subject number: ');
-    ex.visit     = input('Visit number: ');
+    ex.subject   = input('Subject number: ');
+    ex.session   = input('Visit number: ');
     ex.stage     = lower(input('Experiment stage: ','s'));
 end
 
-% Constrain visit number? e.g. comment if not. We have max 2 visits
-assert(ex.visit == 1 || ex.visit == 2, 'Visit number can only be 1 or 2.')
+% Constrain session number? Comment out if not. We have max 2 visits
+assert(ex.session == 1 || ex.session == 2, 'Session number can only be 1 or 2.')
 
 % Constrain possible strings for experiment stage (to prevent 
 % irregularities in output file names)
 stages = {'practice', 'choice', 'perform'};
-try
-    assert(ismember(lower(ex.stage),stages), '\nUnknown experiment stage. Check spelling?\n Possible stages: practice, choice, perform.\n', false)
-catch ME
-    disp(ME.message)
-    ex.stage     = input('Experiment stage: ','s');
+while ~ismember(lower(ex.stage),stages)
+    try
+        assert(ismember(lower(ex.stage),stages), '\nUnknown experiment stage. Check spelling?\n Possible stages: practice, choice, perform.\n', false)
+    catch ME
+        disp(ME.message)
+        ex.stage     = input('Experiment stage: ','s');
+    end
 end
 
-% Output file name
-% Using different naming format
+% Output files
+%   BIDS-like naming scheme
 % -------------------------------------------------------------------------
-% Apparently different output files are used, one collecting all sessions
-% and appending each in one file, another using separate files per session,
-% and a third for the payout.
-% Keeping this setup for now, but will look into reducing to one outputfile
+% Output file of the current session (visit) and experiment stage
+ex.files.output_session_stage = fullfile(ex.dirs.output,sprintf('subject-%.3d_ses-%d_task-EBDM_stage-%s.tsv', ex.subject,ex.session,ex.stage));
 
-% Output file concatenating sessions (make .tsv as output is saved tab
-% separated)
+% Output file of the current session (visit) with all experiment stages
+ex.files.output_session = fullfile(ex.dirs.output,sprintf('subject-%.3d_ses-%d_task-EBDM.tsv', ex.subject,ex.session));
+
+% Output file with all sessions (visits) and experiment stages concatenated
+ex.files.output_all = fullfile(ex.dirs.output,sprintf('subject-%.3d_task-EBDM.tsv', ex.subject));
+
+% Output file of reward payout (per session)
+ex.files.output_payout = fullfile(ex.dirs.output,sprintf('subject-%.3d_ses-%d_task-EBDM_desc-payout.tsv', ex.subject,ex.session));
+
+% Check for existing output files
 % .........................................................................
-ex.outputFilenameSessions = fullfile(ex.outputFolder,sprintf('subject-%.3d_visit-%d_stage-%s.tsv', ex.subjectId,ex.visit,ex.stage));
-
-% Output file for current session. Keeping this because the code currently
-% works with a session number to track instances that the task was started.
-%
-% Will likely remove unnecessary session numbers, and replace with a
-% warning that the subject-visit-stage combination already exists.
-% .........................................................................
-% First get new session number, based on existing output files in the
-% output folder
-
-% Get existing output files
-outfiles = dir(fullfile(ex.outputFolder, strrep(ex.outputFilenameSessions,'.tsv','_ses-*.tsv')));
-outfiles = {outfiles(:).name}';
-
-% Isolate session numbers from file names, and convert from str to numbers
-sesnrs = cellfun(@(x) regexp(x, 'ses-\d+', 'match'), outfiles);
-if ~isempty(sesnrs)
-    sesnrs = cellfun(@(x) str2num(strrep(x, 'ses-', '')), sesnrs);
-else
-    sesnrs = 0;
+% Ask whether to abort in case of existing output file
+if exist(ex.files.output_session_stage,'file') == 2
+    warning('An output file for this participant and experiment stage already exists! Check whether yout session info input is correct.\n  %s',  ...
+        ex.files.output_session_stage)
+    cont = input('Continue anyway?  (yes/no)\n  The results of this run will be appended to existing output files.', 's');
+    cont = strtrim(cont);
+    if ~ismember(lower(cont),{'y', 'yes', 'ja', 'j'})
+        error('\nExperiment settings not accepted. Run %s again to specify different settings.', mfilename);
+    end
 end
-
-% New session number is max of existing sesnrs + 1
-ex.session = max(sesnrs) + 1;
-
-% Session specific output file name for this session
-ex.outputFilenameSession = fullfile(ex.outputFolder,sprintf('subject-%.3d_visit-%d_stage-%s_ses-%d.tsv', ex.subjectId,ex.visit,ex.stage,ex.session));
-
-% Another separate file seems to track the payout of obtained rewards in
-% the performance stage
-% .........................................................................
-ex.payoutFilenameSessions = fullfile(ex.outputFolder, sprintf('subject-%.3d_visit-%d_payout.tsv',ex.subjectId,ex.visit));
 
 % Display session information and ask for confirmation
 % -------------------------------------------------------------------------
-fprintf('\nSubject: %.3d \nVisit: %d \nExperiment stage: %s\nSession: %d\n',ex.subjectId,ex.visit,ex.stage,ex.session);
+fprintf('\nSubject: %.3d \nVisit: %d \nExperiment stage: %s\nSession: %d\n',ex.subject,ex.visit,ex.stage,ex.session);
 accept = input('Is this correct (yes/no)? ', 's');
 accept = strtrim(accept);
-if ~ismember(accept,{'y', 'yes', 'ja', 'j'})
+if ~ismember(lower(accept),{'y', 'yes', 'ja', 'j'})
     error('\nExperiment settings not accepted. Run %s again to specify different settings.', mfilename);
 end
 
-% Load common settings and initialize gripforce (if applicable)
+% Load common settings
+%   Also initializes bitsi for gripforce and MRI, if applicable
 % -------------------------------------------------------------------------
 ex = commonSettings(ex);
 
 % Start task
 % -------------------------------------------------------------------------
-% Display instructions?
-
-% Start task
 params = []; % Empty now. Possible to implement such that it can be used to restore options from earlier sessions
 result = AGT_CoreProtocol_RU_BSI(params,ex);
 
