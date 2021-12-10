698d510d919df5c474de6a70fc60b54670f4b77b
rubenvandenbosch <ruben.vdenbosch@gmail.com>
2021-12-10 13:58:22 +0100

    Fixed bugs in number of repetition and final trial selection. And added field for whether trial offer was accepted

--- functions/getPerformTrials.m
+++ functions/getPerformTrials.m
@@ -47,11 +47,11 @@ for icomb = 1:size(combs,1)
     trIxs{icomb} = find(choicesREixs(1,:) == combs(icomb,1) & choicesREixs(2,:) == combs(icomb,2));
 end
 
-% Get the number of repetitions possible for all reward/effort
-% combinations, given the requested number of perform trials and number of
-% combinations.
+% Get the number of possible repetitions per reward/effort combination for
+% the perform trials, given the requested number of trials and number of
+% combinations
 nTrials = ex.blocks * ex.blockLen;
-nRepsAll = floor(size(combs,1)/nTrials);
+nRepsAll = floor(nTrials/size(combs,1));
 
 % Select equal number of random choices for each combination.
 %   Missing combinations in choices are skipped
@@ -83,11 +83,17 @@ if numel(selected) < nTrials
     selected = [selected, remainingTrials(randsample(numel(remainingTrials),nToSelect,false))];
 end
 
-% Get trial info for the selected trial indices, and shuffle trial order
+% Get trial info for the selected trial indices
+%   Reshape choice trial info to get all in one row (same as selected var)
+%   Add field that indicates whether the trial's offer was accepted
+choices.result.trials = reshape(choices.result.trials',[1,numel(choices.result.trials)]);
 allTrials = choices.result.trials(selected);
-allTrials = allTrials(randperm(numel(allTrials)));
+for ix = 1:numel(selected)
+    allTrials(ix).Yestrial = choices.result.data(selected(ix)).Yestrial;
+end
 
-% Collect trials in a block by trial struct array and return
+% Shuffle trial order and collect trials in a block by trial struct array
+allTrials = allTrials(randperm(numel(allTrials)));
 for b = 1:ex.blocks
     trials(b,:) = allTrials((b-1)*ex.blockLen + 1:(b-1)*ex.blockLen + ex.blockLen);
 end
