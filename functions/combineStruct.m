d8e1a4f2a9425bea3b34c0cc556b6d89464c638d
rubenvandenbosch <ruben.vdenbosch@gmail.com>
2022-01-06 13:56:03 +0100

    Updated combineStruct function

--- functions/combineStruct.m
+++ functions/combineStruct.m
@@ -1,6 +1,12 @@
-function a1=combineStruct(a1, a2)
-%copy all fields in a2 into a1, overwriting any identically named fields in a1
-f=fieldnames(a2);
-for i=1:length(f)
-    a1=setfield(a1, f{i}, getfield(a2, f{i}));
-end;
+function a1 = combineStruct(a1, a2)
+% a1 = combineStruct(a1, a2)
+% Copy all fields of struct a2 into struct a1, overwriting any identically 
+% named fields in a1.
+%
+
+assert(isstruct(a1) && isstruct(a2), 'Input arguments should be of class struct');
+
+fields = fieldnames(a2);
+for ix = 1:numel(fields)
+    a1.(fields{ix}) = a2.(fields{ix});
+end
